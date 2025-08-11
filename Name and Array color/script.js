5b01792c87d49ef5bb8eca71a96372e9f9196e93
Joshualibriro <joshua.libriro@hcdc.edu.ph>
2025-08-11 16:41:25 +0800

    Add files via upload

---
+++ Name and Array color/script.js
@@ -0,0 +1,24 @@
+var Name = "Joshua Libriro";
+let Age = "19";
+const PI = 3.1416;
+let isStudent = Number(Age) < 25 ? true : false;
+
+let person = {
+    name: Name,
+    age: Age,
+    address: {
+        street: "Malagamot Panacan",
+        city: "Davao City",
+        country: "Philippines"
+    },
+    hobbies: ["gaming", "sleeping", "eating"]
+};
+
+let colors = ["red", "black", "green", "white", "black"];
+
+console.log(Name);
+console.log(Age);
+console.log(PI);
+console.log(isStudent);
+console.log(person);
+console.log(colors);
